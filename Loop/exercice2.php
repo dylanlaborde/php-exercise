934a6e3eedaaff05c438fb0c8dee111b98fe17d8
Dylan <dylan4047@gmail.com>
2017-01-09 15:15:20 +0100

    exercice 2 terminer

---
+++ Loop/exercice2.php
@@ -0,0 +1,19 @@
+<h3>Exercice 2 :</h3>
+<?php 
+
+$a = 0;
+$b = 12;
+
+for (; ;) {
+    if ($a >=30 ) { 	
+     echo "a * b = ".$a * $b ."/";
+
+    // echo "<li>"."a * b = ".$a * $b ."</li>";
+
+
+    }
+    if ($a >40 ) {
+        break;
+    }
+    $a++;
+}
