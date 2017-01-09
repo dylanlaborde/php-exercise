d8a40e6226753eca8bcba1ed1adaba8bb557891f
Dylan <dylan4047@gmail.com>
2017-01-09 17:56:11 +0100

    ajout du require exercice4

--- Loop/index.php
+++ Loop/index.php
@@ -7,5 +7,7 @@ error_reporting(E_ALL);
 require __DIR__. '/../Loop/exercice1.php';
 require __DIR__. '/../Loop/exercice2.php';
 require __DIR__. '/../Loop/exercice3.php';
+require __DIR__. '/../Loop/exercice4.php';
+
 
 
