3842c73bc5d81506bf06c88cfd8a3164d00facd8
Dylan <dylan4047@gmail.com>
2017-01-09 15:14:53 +0100

    git fail

--- Loop/index.php
+++ Loop/index.php
@@ -4,7 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__. '/exercice1.php';
-require __DIR__. '/exercice2.php';
-
+require __DIR__. '/../Loop/exercice1.php';
+require __DIR__. '/../Loop/exercice2.php';
 
