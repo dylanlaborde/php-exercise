2356d579779f4cd89839b63ff284441e8e2b926e
Dylan <dylan4047@gmail.com>
2017-01-10 09:24:26 +0100

    fichier index.html changer en .php

--- Function/index.php
+++ Function/index.php
@@ -5,6 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require __DIR__. '/../Function/exercice1.php';
+echo __DIR__. '/../Function/exercice1.php';
 require __DIR__. '/../Function/exercice2.php';
 require __DIR__. '/../Function/exercice3.php';
 require __DIR__. '/../Function/exercice4.php';
