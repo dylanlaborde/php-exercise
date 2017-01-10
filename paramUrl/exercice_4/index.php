6746d25898ffb97d6d60f5373c3017ff0c613b02
Dylan <dylan4047@gmail.com>
2017-01-10 16:59:48 +0100

    exercice terminer

---
+++ paramUrl/exercice_4/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_GET["langage"])) {
+	// echo $_GET["langage"];
+	echo "\n".$_GET["langage"]."\n";
+
+}
+if (!isset($_GET["langage"])) {
+	echo "404 not found";
+}
+if (isset($_GET["serveur"])) {
+	echo "\n".$_GET["serveur"]."\n";
+}
+if (!isset($_GET["serveur"])) {
+	echo "404 not found";
+
+}
+
+
+// index.php?langage=PHP&serveur=LAMP
+ ?>
