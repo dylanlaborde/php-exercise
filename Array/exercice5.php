a8dc8aabc519259c43482c0dca8840a19145a18c
Dylan <dylan4047@gmail.com>
2017-01-10 14:07:40 +0100

    ajout des clé entre guilemet

--- Array/exercice5.php
+++ Array/exercice5.php
@@ -1,6 +1,6 @@
 <h3>Exercice 5</h3>
 <?php 
-$tab = [02 => "Aisne",59 => "Nord",60 => "Oise",62 => "Pas-de-Calais",80 => "Somme" ];
+$tab = ["02" => "Aisne","59" => "Nord","60" => "Oise","62" => "Pas-de-Calais","80" => "Somme" ];
 print_r($tab);
  ?>
 
