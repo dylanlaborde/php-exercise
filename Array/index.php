f56f9b7ed13666f7c535357339b8625968c1d9f0
Dylan <dylan4047@gmail.com>
2017-01-10 10:57:19 +0100

    creation des fichier et ajout des require dans index

---
+++ Array/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__. '/../Array/exercice1.php';
+require __DIR__. '/../Array/exercice2.php';
+require __DIR__. '/../Array/exercice3.php';
+require __DIR__. '/../Array/exercice4.php';
+require __DIR__. '/../Array/exercice5.php';
+require __DIR__. '/../Array/exercice6.php';
+require __DIR__. '/../Array/exercice7.php';
+require __DIR__. '/../Array/exercice8.php';
+
+
