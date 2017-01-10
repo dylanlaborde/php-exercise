dcf1895d455a777fd0665aab67ae5c1661007356
Dylan <dylan4047@gmail.com>
2017-01-10 14:31:06 +0100

    exercice terminer

---
+++ Array/exercice10.php
@@ -0,0 +1,10 @@
+<h3>Exercice 10 :</h3>
+
+<?php 
+
+foreach ($tab as $key => $value) {
+	print_r("<li>"."Le département"."\n".$value."\n"."a le numéro"."\n".$key."</li>");
+}
+
+ ?>
+
---
+++ Array/exercice9.php
@@ -0,0 +1,11 @@
+<h3>Exercice 9 :</h3>
+
+<?php 
+
+foreach ($tab as $value) {
+	print_r($value."\n");
+}
+
+
+
+ ?>
