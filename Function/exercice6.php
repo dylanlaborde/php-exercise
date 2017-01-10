f2cdb675780fc03b611289d3efb60bb8a2b3f718
Dylan <dylan4047@gmail.com>
2017-01-10 10:26:37 +0100

    exercice 6 terminer

---
+++ Function/exercice6.php
@@ -0,0 +1,15 @@
+<h3>Exercice 6</h3>
+
+<?php 
+
+function hello($nom,$prenom,$age){
+
+
+echo "Bonjours"."\n".$nom."\n".$prenom."\n"."tu as \n".$age."ans";
+
+
+}
+
+hello("jean","Bernard","25");
+
+ ?>
