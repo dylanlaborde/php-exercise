db5256a1b9ac69616122f69ae7a3fc4590f6f86c
Dylan <dylan4047@gmail.com>
2017-01-11 10:54:57 +0100

    exercice 2 terminer

---
+++ Form/exercice2/exercice2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice2</title>
+</head>
+<body>
+	<form action="/user.php" method="POST">
+		<div>
+			<label for="nom">nom</label>
+			<input type="text" name="nom">
+		</div>
+		<div>
+			<label for="prenom">prénom</label>
+			<input type="text" name="prenom">
+		</div>
+		<div>
+			<button type="submit">Valider</button>
+		</div>
+	</form>
+</body>
+</html>
