d53b594dc8d3802301779537444bb354d55de230
Dylan <dylan4047@gmail.com>
2017-01-11 14:26:40 +0100

    rectification !!!  formulaire include de l'exercice 5 et rajout de condition pour reinclure le formulaire si les champ sont vide au post

--- Form/exercice6/index.php
+++ Form/exercice6/index.php
@@ -1,11 +1,7 @@
 <?php 
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,31 +10,23 @@ error_reporting(E_ALL);
 	<title>Document</title>
 </head>
 <body>
-	<?php if( isset( $_POST['nom'] ) ) { ?> 
+	<?php 
+
+	if(isset($_POST['nom']) && !empty($_POST['nom'])  ) { ?> 
+
 	<div>
 		<h2><?php echo "Bienvenue"."\n".$_POST['genre']."\n".$_POST['nom']."\n".$_POST['prenom'];?></h2>
 	</div>
-	<?php } else { ?> 
-	<form action="/index.php" method="POST">
-		<div>
-			<label for="genre">Civilité</label>
-			<select name="genre" id="">
-				<option value="Mr">Mr</option>
-				<option value="Mme">Mme</option>
-			</select>
-		</div>
-		<div>
-			<label for="nom">Nom</label>
-			<input type="text" name="nom">
-		</div>
-		<div>
-			<label for="prenom">Prénom</label>
-			<input type="text" name="prenom">
-		</div>
-		<div>
-			<button type="submit">Valider</button>
-		</div>
-	</form>
-	<?php } ?> 
+
+	<?php } 
+
+	if(empty($_POST['nom']) || empty($_GET['nom'])){
+		include __DIR__. '/../exercice5/index.php';
+		
+	}
+	else { 
+		include __DIR__. '/../exercice5/index.php';
+	}?> 
+
 </body>
 </html>
