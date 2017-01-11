ab394ac1e76e77469986110744b0fdd06e4e11ea
Dylan <dylan4047@gmail.com>
2017-01-11 10:47:44 +0100

    ajout du dosier form et exercice 1 terminer

---
+++ Form/exercice1/exercice1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice1</title>
+</head>
+<body>
+	<form action="/user.php" method="GET">
+		<div>
+			<label for="nom">nom</label>
+			<input type="text" name="nom">
+		</div>
+		<div>
+			<label for="prenom">prénom</label>
+			<input type="text" name="prenom">
+		</div>
+		<div>
+			<button type="submit">Valider</button>
+		</div>
+	</form>
+
+</body>
+</html>
