c4edaa7e999c9b39ddec9b999e1af3c35abe16cf
Dylan <dylan4047@gmail.com>
2017-01-13 06:10:25 +0100

    rajout d'une condition isset pour stocker les information que la validation du formulaire

--- GlobalVariable/exercice3.php
+++ GlobalVariable/exercice3.php
@@ -2,8 +2,10 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-setcookie("mail",$_POST["mail"],time() + 3600);
-setcookie("mdp",$_POST["mdp"],time() + 3600);
+if (isset($_POST["mail"]) || isset($_POST["mdp"])) {
+	setcookie("mail",$_POST["mail"],time() + 3600);
+	setcookie("mdp",$_POST["mdp"],time() + 3600);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
