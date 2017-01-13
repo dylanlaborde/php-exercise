e6f70db234c247dc129cce1cad02abf4526d756f
Dylan <dylan4047@gmail.com>
2017-01-13 03:31:29 +0100

    ajout des page pour chaque exercice contenant un menu et le nom de la page

---
+++ GlobalVariable/exercice4.php
@@ -0,0 +1,27 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercice 4</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<menu>
+		<ul class="nav nav-tabs">
+			<li role="presentation"><a href="/exercice1.php">exercice 1</a></li>
+			<li role="presentation"><a href="/exercice2.php">exercice 2</a></li>
+			<li role="presentation"><a href="/exercice3.php">exercice 3</a></li>
+			<li role="presentation" class="active"><a href="/exercice4.php">exercice 4</a></li>
+			<li role="presentation"><a href="/exercice5.php">exercice 5</a></li>
+		</ul>
+	</menu>
+	<h3>exerice 4 :</h3>
+
+
+</body>
+</html>
